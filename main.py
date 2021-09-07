4048ba4009beaa42549e5c28d6943c8f530e3f8c
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-09-07 18:25:36 +0900

    Feat: 메세지 입력을 받는 로직 추가
    채널에 전송된 메시지를 받고 모델 돌려보는 기본적인 코드를 구현했습니다.

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import os
+import asyncio
+import requests
+import discord
+
 from config import Config
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.author.id == client.user.id:
+        return
+    url = os.path.join(Config.API_URL, 'text', message.content)
+    response = requests.get(url)
+    await message.channel.send(response.text)
+
+client.run(Config.TOKEN)
